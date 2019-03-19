3319e064bc4918165e6a965834b149ee0340f136
Eric Minor <ermi1253@colorado.edu>
2019-03-18 23:07:46 -0600

    added video to frame converter. Simplified workflow

---
+++ opencv-face-recognition/regenerateAndRun.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 22:51:31 2019
+
+@author: Eric
+"""
+import os
+#execfile("extract_embeddings.py --dataset dataset --embeddings output/embeddings.pickle --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7")
+os.system("python extract_embeddings.py --dataset dataset --embeddings output/embeddings.pickle --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7")
+os.system("python train_model.py --embeddings output/embeddings.pickle --recognizer output/recognizer.pickle --le output/le.pickle")
+os.system("python recognize_video.py --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle")
---
+++ video_to_frames/vidFrames.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 22:19:06 2019
+
+@author: Eric
+"""
+
+#change personName to the person's name
+#change vidcap string to the video name (should be in same folder)
+#change path to the location of the dataset.
+
+import cv2
+
+personName = "Eric"
+path = "E:\Class Work\HCI\Final Project\FinalProject-Marauders_Map\opencv-face-recognition\dataset\\"
+vidcap = cv2.VideoCapture('Eric.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+    
+    img2 = cv2.rotate(image,cv2.ROTATE_90_COUNTERCLOCKWISE);
+  
+    cv2.imwrite("%s%s/frame%d.jpg" % (path,personName,count), img2)     # save frame as JPEG file      
+    success,image = vidcap.read()
+    print('Read a new frame: ', success)
+    count += 1
+  
+vidcap.release()
